c65d4cc0b61ce252094762ed74752987b4e626e1
Bilal Muhammad <bmuhamm@gmail.com>
2026-07-21 17:31:41 +0700

    missed codes

--- src/bin/box1.rs
+++ src/bin/box1.rs
@@ -1,2 +1,23 @@
+#[derive(Debug)]
+enum List {
+    Cons(i32, Box<List>),
+    Nil
+}
+use List::{Cons, Nil};
 fn main() {
+    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+    println!("{:?}", list);
+    println!("-----------");
+    print_all(&list);
+}
+
+fn print_all(list: &List) {
+    match list {
+        Cons(value, rest) => {
+            print!("value: {:?}", value);
+            print!(" rest: {:?} ", rest);
+            print_all(rest);
+        },
+        Nil => {},
+    }
 }
