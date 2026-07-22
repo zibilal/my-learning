247dad7b2c1d0ba15ea92ae641922df9284222e6
zibilal <bmuhamm@gmail.com>
2026-07-22 07:49:52 +0700

    Updating chat server

---
+++ src/bin/chat_server_include_address.rs
@@ -0,0 +1,37 @@
+use tokio::{
+    io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
+    net::TcpListener,
+    sync::broadcast,
+};
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").await.unwrap();
+    let (tx, _) = broadcast::channel(10);
+    loop {
+        let tx = tx.clone();
+        let mut rx = tx.subscribe();
+        let (mut socket, address) = listener.accept().await.unwrap();
+        tokio::spawn(async move {
+            let (stream_reader, mut stream_writer) = socket.split();
+            let mut message = String::new();
+            let mut reader = BufReader::new(stream_reader);
+            loop {
+                tokio::select! {
+                    result = reader.read_line(&mut message) => {
+                        if result.unwrap() == 0 {
+                            break;
+                        }
+                        tx.send((message.clone(), address)).unwrap();
+                        message.clear();
+                    }
+                    result = rx.recv() => {
+                        let (received_message, sender_address) = result.unwrap();
+                        if address != sender_address {
+                            stream_writer.write_all(received_message.as_bytes()).await.unwrap();
+                        }
+                    }
+                }
+            }
+        });
+    }
+}
